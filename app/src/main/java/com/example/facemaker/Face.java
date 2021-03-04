7e8e5de8324d5da836b81770ff8b97e9709797a5
dewey <a874589@gmail.com>
2021-03-04 00:18:28 -0800

    Finished Face functionality

--- app/src/main/java/com/example/facemaker/Face.java
+++ app/src/main/java/com/example/facemaker/Face.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceView;
 import androidx.annotation.RequiresApi;
 import java.util.Random;
@@ -30,9 +33,30 @@ public class Face extends SurfaceView {
     //Bounds of the Surface View
     Rect bounds = new Rect();
 
+    //Constants to describe how face is drawn
+    private static final float faceWidth = 1000.0f;
+    private static final float foreheadHeight = 400.0f;
+    private static final float faceHeight = 1300.0f;
+    private static final float chinHeight = 400.0f;
+    private static final float chinWidth = 400.0f;
+    private static final float[] eyePosition = {0.0f, -50.0f};
+    private static final float eyeSeperation = 250.0f;
+    private static final float eyeWidth = 75.0f;
+    private static final float eyeHeight = 50.0f;
+    private static final float pupilSize = 30.0f;
+    private static final float[] nosePosition = {0.0f, 120.0f};
+    private static final float noseSize = 40.0f;
+    private static final float[] mouthPosition = {0.0f, 400.0f};
+    private static final float mouthWidth = 200.0f;
+
+    private static final float x = 400.0f;
+    private static final float y = 400.0f;
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     public Face(Context context, AttributeSet attrs){
 
         super(context, attrs);
+        setWillNotDraw(false);
         randomize();
 
         //Initializes face paints
@@ -47,18 +71,26 @@ public class Face extends SurfaceView {
         featuresPaint.setColor(Color.BLACK);
         featuresPaint.setStrokeWidth(15);
 
+        //Sets background color
+        setBackgroundColor(Color.WHITE);
+
     }
 
     //Randomize face details
     private void randomize(){
-        this.model.skinColor = numGenerator.nextInt(16777216);
-        this.model.eyeColor = numGenerator.nextInt(16777216);
-        this.model.hairColor = numGenerator.nextInt(16777216);
-        this.model.hairStyle = numGenerator.nextInt(4);
+
+        model.skinColor = numGenerator.nextInt(16777216);
+        model.skinColor += 0xFF000000;
+        model.eyeColor = numGenerator.nextInt(16777216);
+        model.eyeColor += 0xFF000000;
+        model.hairColor = numGenerator.nextInt(16777216);
+        model.hairColor += 0xFF000000;
+        model.hairStyle = numGenerator.nextInt(4);
+
     }
 
     @Override
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void onDraw(Canvas canvas) {
 
         //Sets paints to the correct color
@@ -66,10 +98,6 @@ public class Face extends SurfaceView {
         irisPaint.setColor(model.eyeColor);
         hairPaint.setColor(model.hairColor);
 
-        //Gets the bounds of the Surface View
-        setClipBounds(bounds);
-
-        //Draws face by calling helper functions
         drawHairBack(canvas);
         drawFaceShape(canvas);
         drawEyes(canvas);
@@ -78,42 +106,139 @@ public class Face extends SurfaceView {
 
     }
 
+
     //Draws the hair that is behind your hair (as in long hair)
     private void drawHairBack(Canvas canvas){
         switch(model.hairStyle) {
             case 2:
-
+                canvas.drawRect(x, y + faceHeight - chinHeight,
+                        x + faceWidth, y + faceHeight * 1.1f, hairPaint);
         }
     }
 
     //Draws the hair in front of your head
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void drawHairFront(Canvas canvas){
         switch(model.hairStyle) {
+
+            //Bowl Cut
+            case 0:
+                canvas.drawArc(x - faceWidth * 0.05f, y - foreheadHeight * 0.1f,
+                        x + faceWidth * 1.05f, y + foreheadHeight * 2.1f,
+                        0f, -180f, true, hairPaint);
+                break;
+
+            //Spiky Hair
             case 1:
-
+                //This is more complicated so we call a helper function
+                drawSpikyHair(canvas, 10, 100.0f);
                 break;
 
+            //Long Hair
             case 2:
-
+                canvas.drawArc(x, y, x + faceWidth, y + foreheadHeight * 2,
+                        0f, -180f, true, hairPaint);
                 break;
-
-            case 3:
 
         }
     }
 
+    private void drawSpikyHair(Canvas canvas, int spikes, float spikeHeight){
+
+        double increment = Math.toRadians(180) / spikes;
+
+        Path p = new Path();
+        p.moveTo(x + faceWidth, y + foreheadHeight);
+
+        for(double angle = 0; angle < Math.toRadians(180); angle += increment){
+            p.lineTo(x + (float) ((1 + spikeHeight/faceWidth) * Math.cos(angle + increment/2) + 1) * faceWidth/2,
+                    y - (float) ((1 + spikeHeight/foreheadHeight) * Math.sin(angle + increment/2) - 1) * foreheadHeight);
+            p.lineTo(x + (float) (Math.cos(angle + increment) + 1) * faceWidth/2,
+                    y - (float) (Math.sin(angle + increment) - 1) * foreheadHeight);
+        }
+
+        p.close();
+
+        canvas.drawPath(p, hairPaint);
+
+    }
+
     //Draw the general shape of your face
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void drawFaceShape(Canvas canvas){
 
+        Path p = new Path();
+        p.moveTo(x, y + faceHeight - chinHeight);
+        p.lineTo(x, y + foreheadHeight);
+        p.arcTo(x, y, x + faceWidth, y + 2 * foreheadHeight,
+                0, -180, false);
+        p.lineTo(x + faceWidth, y + foreheadHeight);
+        p.lineTo(x + faceWidth, y + faceHeight - chinHeight);
+        p.lineTo(x + faceWidth / 2 + chinWidth / 2, y + faceHeight);
+        p.lineTo(x + faceWidth / 2 - chinWidth / 2, y + faceHeight);
+        p.close();
+
+        canvas.drawPath(p, skinPaint);
     }
 
     //Draws features on your face (nose and mouth)
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void drawFaceFeatures(Canvas canvas){
 
+        //Draws nose
+        canvas.drawArc(x + faceWidth / 2 + nosePosition[0] - noseSize,
+                y + faceHeight / 2 + nosePosition[1] - noseSize,
+                x + faceWidth / 2 + nosePosition[0] + noseSize,
+                y + faceHeight / 2 + nosePosition[1] + noseSize,
+                90, 180, false, featuresPaint);
+
+        //Draws mouth
+        canvas.drawLine(x + faceWidth/2 + mouthPosition[0] - mouthWidth/2,
+                        y + faceHeight/2 + mouthPosition[1],
+                        x + faceWidth/2 + mouthPosition[0] + mouthWidth/2,
+                        y + faceHeight/2 + mouthPosition[1],
+                        featuresPaint);
     }
 
     //Draws your eyes
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void drawEyes(Canvas canvas){
+
+        //draws whites of eyes
+        canvas.drawOval(x + faceWidth/2 + eyePosition[0] - eyeSeperation - eyeWidth,
+                y + faceHeight/2 + eyePosition[1] - eyeHeight,
+                x + faceWidth/2 + eyePosition[0] - eyeSeperation + eyeWidth,
+                y + faceHeight/2 + eyePosition[1] + eyeHeight,
+                eyeWhitePaint);
+        canvas.drawOval(x + faceWidth/2 + eyePosition[0] + eyeSeperation - eyeWidth,
+                y + faceHeight/2 + eyePosition[1] - eyeHeight,
+                x + faceWidth/2 + eyePosition[0] + eyeSeperation + eyeWidth,
+                y + faceHeight/2 + eyePosition[1] + eyeHeight,
+                eyeWhitePaint);
+
+        //draws irises
+        canvas.drawCircle(x + faceWidth/2 + eyePosition[0] - eyeSeperation,
+                y + faceHeight/2 + eyePosition[1], eyeHeight, irisPaint);
+        canvas.drawCircle(x + faceWidth/2 + eyePosition[0] + eyeSeperation,
+                y + faceHeight/2 + eyePosition[1], eyeHeight, irisPaint);
+
+        //draws pupils
+        canvas.drawCircle(x + faceWidth/2 + eyePosition[0] - eyeSeperation,
+                y + faceHeight/2 + eyePosition[1], pupilSize, pupilPaint);
+        canvas.drawCircle(x + faceWidth/2 + eyePosition[0] + eyeSeperation,
+                y + faceHeight/2 + eyePosition[1], pupilSize, pupilPaint);
+
+        //draw outline of eyes
+        canvas.drawOval(x + faceWidth/2 + eyePosition[0] - eyeSeperation - eyeWidth,
+                        y + faceHeight/2 + eyePosition[1] - eyeHeight,
+                        x + faceWidth/2 + eyePosition[0] - eyeSeperation + eyeWidth,
+                        y + faceHeight/2 + eyePosition[1] + eyeHeight,
+                        featuresPaint);
+        canvas.drawOval(x + faceWidth/2 + eyePosition[0] + eyeSeperation - eyeWidth,
+                        y + faceHeight/2 + eyePosition[1] - eyeHeight,
+                        x + faceWidth/2 + eyePosition[0] + eyeSeperation + eyeWidth,
+                        y + faceHeight/2 + eyePosition[1] + eyeHeight,
+                        featuresPaint);
 
     }
 
